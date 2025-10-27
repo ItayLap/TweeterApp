7749845ef636d3ac93804d6ba547563339a6a913
Itay Laptev <itaylaptev@gmail.com>
2025-10-27 18:07:54 +0200

    Update

---
+++ TweeterApp/Migrations/20251022160232_FriendsUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TweeterApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FriendsUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RespondedAt",
+                table: "Friends",
+                newName: "UpdatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Friends",
+                newName: "RespondedAt");
+        }
+    }
+}
